9ff9248377ca7fa5c7fb01230682ec76e93e7a32
Firekeeper28 <gkwamenkrumah28@gmail.com>
2025-08-03 07:49:15 +0100

    Update ComingSoon.jsx

--- src/section/ComingSoon.jsx
+++ src/section/ComingSoon.jsx
@@ -6,7 +6,7 @@ const ComingSoon = () => {
 
         <div className="text-wrapper">
           <h3 className="gradient-title">
-            Coming <br /> May 26th <br /> 2026
+            First <br /> Launched in<br /> 2018
           </h3>
         </div>
 
